fb03d60749445a06cfca74107b5411acbd19e6ad
miracleeze <miracleeze221@gmail.com>
2021-10-28 11:24:51 -0600

    update all

--- TeamActivityW05/hikeModel.js
+++ TeamActivityW05/hikeModel.js
@@ -1,4 +1,5 @@
 // normally the model would have more going on...retrieving the hikes from a database, filtering, etc. Our model will be very simple.  We could simply export the hikeList, but a better pattern would be to create a 'getter' function to do it instead. That way as our model changed...we could simply change the getter function and anything using it should be able to remain the same.
+
 const hikeList = [
   {
     name: "Bechler Falls",
