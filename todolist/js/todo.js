ced28a0d2d0d5b151d9ce1999332febc11e560de
miracleeze <miracleeze221@gmail.com>
2021-10-27 22:49:24 -0600

    update all

--- todolist/js/todo.js
+++ todolist/js/todo.js
@@ -7,6 +7,30 @@ const CHECK = "fa-chec-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
+let LIST, id;
+
+let data = localStorage.getItem("TODO");
+
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadList(LIST);
+} else {
+  LIST = [];
+  id = 0;
+}
+
+function loadList(array) {
+  array.forEach(function (item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+}
+
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
+
 const options = { weekday: "long", month: "short", day: "numeric" };
 const today = new Date();
 
@@ -18,30 +42,6 @@ function addToDo(toDo, id, done, trash) {
   }
   const DONE = done ? CHECK : UNCHECK;
   const LINE = done ? LINE_THROUGH : "";
-
-  let LIST, id;
-
-  let data = localStorage.getItem("TODO");
-
-  if (data) {
-    LIST = JSON.parse(data);
-    id = LIST.length;
-    loadList(LIST);
-  } else {
-    LIST = [];
-    id = 0;
-  }
-
-  function loadList(array) {
-    array.forEach(function (item) {
-      addToDo(item.name, item.id, item.done, item.trash);
-    });
-  }
-
-  clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
-  });
 
   const item = `<li class="item">
             <i class="fa ${DONE} co" job="complete" id="${id}"></i>
@@ -73,15 +73,16 @@ document.addEventListener("keyup", function (even) {
   }
 });
 
-function completeToDo() {
+function completeToDo(element) {
   element.classList.toggle(CHECK);
   element.classList.toggle(UNCHECK);
   element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
   LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
 function removeToDo(element) {
-  element.parentNode.parenNode.removeChild(element.parenNode);
+  element.parentNode.parentNode.removeChild(element.parentNode);
 
   LIST[element.id].trash = true;
 }
